9395d5a915c9b8910ad154df0c5e1ee21b532095
Ishaan <ishaan10dutta@gmail.com>
2024-04-15 23:40:23 +0100

    getting there.

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 const Page = () => {
   const router = useRouter();
@@ -37,9 +37,11 @@ const Page = () => {
   return (
     <div className="w-full mt-24 flex justify-center">
       <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account...</h3>
-        <p>You will be redirected automatically.</p>
+        <Suspense
+          fallback={<Loader2 className="h-8 w-8 animate-spin text-zinc-800" />}>
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </Suspense>
       </div>
     </div>
   );
