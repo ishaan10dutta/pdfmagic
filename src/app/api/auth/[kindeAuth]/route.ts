af375f4001a1e80640b256c42af122e6bfc12c58
Ishaan <ishaan10dutta@gmail.com>
2024-04-15 18:24:41 +0100

    Part 4.

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint) as Promise<any>;
+  return handleAuth(request, endpoint);
 }
