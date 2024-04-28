1b58594ffa3f0af72efb3fa328117434b866db86
Ishaan <ishaan10dutta@gmail.com>
2024-04-28 11:01:34 +0100

    Get started button bugfix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,37 @@
-import Image from "next/image";
-import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import { buttonVariants } from "@/app/components/ui/button";
+import Link from "next/link";
 
-export default function Home() {
+import MaxWidthWrapper from "@/app/components/MaxWidthWrapper";
+import { buttonVariants } from "@/app/components/ui/button";
+import Image from "next/image";
+
+const HomePage = () => {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
-            Try the magic now!
+            Quill is now public!
           </p>
         </div>
+
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chat with your <span className="text-blue-600">documents</span> in
           seconds.
         </h1>
+
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          PdfMagic allows you to have conversations with any PDF document. Simply
+          Quill allows you to have conversations with any PDF document. Simply
           upload your file and start asking questions right away.
         </p>
 
         <Link
+          href="/dashboard"
+          target="_blank"
           className={buttonVariants({
             size: "lg",
             className: "mt-5",
-          })}
-          href="/dashboard"
-          target="_blank">
+          })}>
           Get started <ArrowRight className="ml-2 h-5 w-5" />
         </Link>
       </MaxWidthWrapper>
@@ -37,7 +40,7 @@ export default function Home() {
       <div>
         <div className="relative isolate">
           <div
-            aria-hidden="true"
+            aria-hidden
             className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
             <div
               style={{
@@ -66,7 +69,7 @@ export default function Home() {
           </div>
 
           <div
-            aria-hidden="true"
+            aria-hidden
             className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
             <div
               style={{
@@ -79,20 +82,21 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Feature section */}
+      {/* feature section */}
       <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
               Start chatting in minutes
             </h2>
+
             <p className="mt-4 text-lg text-gray-600">
-              Chatting to your PDF files has never been easier than with PdfMagic.
+              Chatting to your PDF files has never been easier than with Quill.
             </p>
           </div>
         </div>
 
-        {/* options */}
+        {/* steps */}
         <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
           <li className="md:flex-1">
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
@@ -111,6 +115,7 @@ export default function Home() {
               </span>
             </div>
           </li>
+
           <li className="md:flex-1">
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-blue-600">Step 2</span>
@@ -123,6 +128,7 @@ export default function Home() {
               </span>
             </div>
           </li>
+
           <li className="md:flex-1">
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-blue-600">Step 3</span>
@@ -130,13 +136,14 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Try out PdfMagic today - it really takes
-                less than a minute.
+                It&apos;s that simple. Try out Quill today - It takes less than
+                a minute.
               </span>
             </div>
           </li>
         </ol>
 
+        {/* upload preview showcase */}
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
           <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
@@ -154,4 +161,6 @@ export default function Home() {
       </div>
     </>
   );
-}
+};
+
+export default HomePage;
