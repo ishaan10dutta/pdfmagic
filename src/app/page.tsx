4f8212c1ea75e9a19237a912a1b75a2d1ed7c393
Ishaan <ishaan10dutta@gmail.com>
2024-04-28 19:41:40 +0100

    CORS fix continued.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
-
 import MaxWidthWrapper from "@/app/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/app/components/ui/button";
 import Image from "next/image";
 
-const HomePage = () => {
+export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
@@ -14,24 +13,22 @@ const HomePage = () => {
             Quill is now public!
           </p>
         </div>
-
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chat with your <span className="text-blue-600">documents</span> in
           seconds.
         </h1>
-
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
           Quill allows you to have conversations with any PDF document. Simply
           upload your file and start asking questions right away.
         </p>
 
         <Link
-          href="/dashboard"
-          target="_blank"
           className={buttonVariants({
             size: "lg",
             className: "mt-5",
-          })}>
+          })}
+          href="/dashboard"
+          target="_blank">
           Get started <ArrowRight className="ml-2 h-5 w-5" />
         </Link>
       </MaxWidthWrapper>
@@ -40,7 +37,7 @@ const HomePage = () => {
       <div>
         <div className="relative isolate">
           <div
-            aria-hidden
+            aria-hidden="true"
             className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
             <div
               style={{
@@ -69,7 +66,7 @@ const HomePage = () => {
           </div>
 
           <div
-            aria-hidden
+            aria-hidden="true"
             className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
             <div
               style={{
@@ -82,14 +79,13 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* feature section */}
+      {/* Feature section */}
       <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
               Start chatting in minutes
             </h2>
-
             <p className="mt-4 text-lg text-gray-600">
               Chatting to your PDF files has never been easier than with Quill.
             </p>
@@ -115,7 +111,6 @@ const HomePage = () => {
               </span>
             </div>
           </li>
-
           <li className="md:flex-1">
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-blue-600">Step 2</span>
@@ -128,7 +123,6 @@ const HomePage = () => {
               </span>
             </div>
           </li>
-
           <li className="md:flex-1">
             <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
               <span className="text-sm font-medium text-blue-600">Step 3</span>
@@ -136,14 +130,13 @@ const HomePage = () => {
                 Start asking questions
               </span>
               <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Try out Quill today - It takes less than
-                a minute.
+                It&apos;s that simple. Try out Quill today - it really takes
+                less than a minute.
               </span>
             </div>
           </li>
         </ol>
 
-        {/* upload preview showcase */}
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
           <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
@@ -161,6 +154,4 @@ const HomePage = () => {
       </div>
     </>
   );
-};
-
-export default HomePage;
+}
